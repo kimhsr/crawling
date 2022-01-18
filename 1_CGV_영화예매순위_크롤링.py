3f57457c30aee95732724641d36e134324ba1996
kimhsr <kimhsr@naver.com>
2022-01-18 21:35:18 +0900

    영화예매1위 가져오기

--- 1_CGV_영화예매순위_크롤링.py
+++ 1_CGV_영화예매순위_크롤링.py
@@ -1,4 +1,16 @@
 import urllib.request as req
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup  # pip install bs4
 
-print("test")
+# 서버한테 HTML 코드 받기
+code = req.urlopen("http://www.cgv.co.kr/movies/")
+# print(code.read())
+
+# HTML 코드 정리하기
+soup = BeautifulSoup(code, "html.parser")
+# print(soup)
+
+# 원하는 요소 찾게하기
+title = soup.select_one("strong.title")
+
+# 요소의 내용 출력하기
+print(title.string)
