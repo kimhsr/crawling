524f716c9a15118a2f9577861061415328a4fc22
kimhsr <kimhsr@naver.com>
2022-01-18 23:26:25 +0900

    영화예매순위크롤링

--- 1_CGV_영화예매순위_크롤링.py
+++ 1_CGV_영화예매순위_크롤링.py
@@ -2,7 +2,7 @@ import urllib.request as req
 from bs4 import BeautifulSoup  # pip install bs4
 
 # 서버한테 HTML 코드 받기
-code = req.urlopen("http://www.cgv.co.kr/movies/")
+code = req.urlopen("http://www.cgv.co.kr/movies/?lt=1&ft=1")
 # print(code.read())
 
 # HTML 코드 정리하기
@@ -10,7 +10,14 @@ soup = BeautifulSoup(code, "html.parser")
 # print(soup)
 
 # 원하는 요소 찾게하기
-title = soup.select_one("strong.title")
+# <CSS 선택자> : 기호를 이용해서 내가 원하는 요소를 알려줌
+# - "." : 속성명이 class
+# - "#" : 속성명이 id
+# - " > " : 자손
+# - " " : 후손
+# title = soup.select_one("ol div.box-contents > a > strong.title") # 요소 하나만 가져옴
+title = soup.select("strong.title") # 요소 여러개 한번에 가져옴
 
 # 요소의 내용 출력하기
-print(title.string)
+for i in title:
+    print(i.string) # 요소 내용 꺼내오기
