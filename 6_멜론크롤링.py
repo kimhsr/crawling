5f49603b98785ec3caf228407e7f9e1814ff4aa9
kimhsr <kimhsr@naver.com>
2022-01-20 17:45:01 +0900

    곡정보,앨범 크롤링

--- 6_멜론크롤링.py
+++ 6_멜론크롤링.py
@@ -1,7 +1,6 @@
 import urllib.request as req
 from bs4 import BeautifulSoup
 
-
 header = req.Request("https://www.melon.com/chart/index.htm", headers={"User-Agent": "Mozilla/5.0"})
 code = req.urlopen(header)
 soup = BeautifulSoup(code, "html.parser")
@@ -10,5 +9,5 @@ name = soup.select("div.ellipsis.rank02 > span")
 album = soup.select("div.ellipsis.rank03 > a")
 img = soup.select("a.image_typeAll > img")
 
-# for i in range(len(title)):
-#     print(title[i].string, name[i].text, album[i].string, img[i].attrs["src"])
+for i in range(len(title)):
+    print(title[i].string, name[i].text, album[i].string, img[i].attrs["src"])
